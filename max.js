c605887481bde7236a31a198903e0b54d365f177
hahriar <closebrocircle@gmail.com>
2021-08-09 22:30:35 +0600

    final final final 3.0 comite

--- max.js
+++ max.js
@@ -1,31 +1,14 @@
-/* function seerToMon(seer){
-    var monQuentity = seer / 37;
-    return monQuentity;
+function totalSales( tshirtPrice, pantPrice, shoesPrice){
+    const perShirt = 100;
+    const perPent = 200;
+    const perShoes = 500;
+    // calculation 
+   const shirtSellPrice = tshirtPrice * perShirt;
+   const pentSellPrice =  pantPrice * perPent;
+   const shoesSellPrice = shoesPrice * perShoes;
+   const totalSell = shirtSellPrice + pentSellPrice + shoesSellPrice;
+   return totalSell;
+
 }
-
-var weight = 500;
-var mon = seerToMon(weight);
-console.log('Mon Quentity', mon); */
-
-
-/* const mynumber = 20;
-const output = Math.random()* 10;
-const rounded = Math.floor(output);
-console.log(rounded) */
-
-
-/* function totalSales(tShirt, pant, shoe){
-    var total = tShirt + pant + shoe;
-    return total;
-}
-var allClothes = (100, 200, 500);
-// var cloth1 = 100;
-// var cloth2 = 200; 
-// var cloth3 = 500;
-// var saleInStore = totalSales(cloth1, cloth2, cloth3);
-var saleInStore = totalSales(allClothes);
-var output = Math.random(saleInStore) * 1000;
-var rounded = Math.floor(output);
-console.log(rounded);  */
-
-
+ const firstOption = totalSales(1, 3, 4);
+ console.log(firstOption);
--- swap.js
+++ swap.js
@@ -1,11 +1,126 @@
-var first = 7;
-var second = 5;
-console.log(first, second);
 
-// var temp = first;
-// var first = second;
-// second = temp;
-// console.log(first, second);
+/* ------------------------------------------------------------------------------------------------------------ */
 
-[first, second] = [second, first];
-console.log(first, second);
+//problem 1 
+
+function seerToMon(seer){
+    // error concept clear
+    if(typeof seer != 'number'){
+        return 'please give me original number'
+    }
+    if(seer < 0){
+        return 'please enter the positive number'
+    }
+    // return mon
+    var mon = seer / 40;
+    return mon;
+}
+
+// output
+
+const totalMon = seerToMon(80);
+console.log(totalMon);
+
+
+
+/* --------------------------------------------------------------------------- */
+
+
+//problem 2
+
+
+function totalSales( tshirtPrice, pantPrice, shoesPrice){
+    const perShirt = 100;
+    const perPent = 200;
+    const perShoes = 500;
+    // calculation 
+   const shirtSellPrice = tshirtPrice * perShirt;
+   const pentSellPrice =  pantPrice * perPent;
+   const shoesSellPrice = shoesPrice * perShoes;
+   const totalSell = shirtSellPrice + pentSellPrice + shoesSellPrice;
+   return totalSell;
+
+}
+
+// output
+
+ const firstOption = totalSales(1, 3, 4);
+ console.log(firstOption);
+
+
+
+ /* ------------------------------------------------------------------------------------------ */
+
+
+
+ //problem 3
+
+ // delivery cost
+
+function deliveryCost(tShirtQuantity) {
+    // check if the quanties are negatives or not
+
+
+    if (tShirtQuantity < 0) {
+        ' return shirt quantity cannot be negative'
+    } else {
+        var cost = 0;
+        if (tShirtQuantity <= 100) {
+            cost = tShirtQuantity * 100;
+        } else if (tShirtQuantity <= 200) {
+            var upto100Quantity = 100 * 100;
+            var remainingQuantity = tShirtQuantity - 100;
+            var uptp200Quantity = 80 * remainingQuantity;
+            cost = upto100Quantity + uptp200Quantity;
+
+        } else {
+            var upto100Quantity = 100 * 100;
+            var uptp200Quantity = 80 * 100;
+            var remainingQuantity = tShirtQuantity - 200;
+            var after200Quantity = 50 * remainingQuantity;
+            cost = upto100Quantity + uptp200Quantity + after200Quantity;
+        }
+    }
+    return cost;
+}
+
+
+var cost = deliveryCost(250);
+console.log(cost);
+
+
+
+
+/* --------------------------------------------------------------------------------------------- */
+
+
+
+//problem 4 
+
+
+
+
+// perfect friend
+// task-04
+function perfectFriend(FriendNames) {
+    // check this array or not!!!
+
+    if (typeof (FriendNames) != 'object') {
+
+        return 'please input a arrayList!!!';
+    } else {
+        for (var i = 0; 1 < FriendNames.length; i++) {
+            var element = FriendNames[i];
+            if (element.length == 5) {
+                return element;
+            }
+        }
+
+    } return "please enter a name with 5-letters;"
+}
+
+var Names = ['shimul', 'fahim', 'raihan', 'Tashsin', 'rity'];
+var bestFriends = perfectFriend(Names);
+console.log(bestFriends);
+
+/* ------------------------------------------------------------------------------------------------ */
